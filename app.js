e8ce473fdc69a465f7616d8633c7013811350b5d
Matias Yogui <myogui@technisys.com>
2021-11-17 14:27:19 -0300

    agrego uso de ajax con api

--- app.js
+++ app.js
@@ -1,214 +1,24 @@
-// MASCOTA OBJETO
-class Mascota {
-  constructor(nombre, tipo, sexo, raza, color, descripcion) {
-    this.nombre = nombre;
-    this.tipo = tipo;
-    this.sexo = sexo;
-    this.raza = raza;
-    this.color = color;
-    this.descripcion = descripcion;
-  }
+const URLepisodio = "https://rickandmortyapi.com/api/episode/";
+$("#episodio").click(() => {
+  $.get(
+    URLepisodio + Math.floor(Math.random() * (52 - 1) + 1),
+    function (res, req) {
+      if (req === "success") {
+        let informacion = res;
+        console.log(informacion);
 
-  cambiarNombre(nuevoNombre) {
-    this.nombre = nuevoNombre;
-  }
-}
+        $("#capitulos").prepend(`
+        <div class="capitulo">
 
-// ARRAY INICIAL
-const mascotasEnAdopcion = [
-  new Mascota(
-    "richard",
-    "gato",
-    "macho",
-    "indefinido",
-    "marron",
-    "gato gris grande :)"
-  ),
-  new Mascota(
-    "reina",
-    "gato",
-    "hembra",
-    "siames",
-    "blanco",
-    "gata marron grande :)"
-  ),
-  new Mascota(
-    "lupe",
-    "perro",
-    "hembra",
-    "caniche",
-    "blanco",
-    "perra marron grande :)"
-  ),
-  new Mascota(
-    "mar",
-    "perro",
-    "hembra",
-    "indefinido",
-    "marron",
-    "perra marro grande :)"
-  ),
-  new Mascota(
-    "piolin",
-    "pajaro",
-    "macho",
-    "canario",
-    "amarillo",
-    "pajaro chiquito amarillo :)"
-  ),
-  new Mascota(
-    "donald",
-    "pajaro",
-    "macho",
-    "pato",
-    "blanco",
-    "pajaro blanco grande :)"
-  ),
-];
-
-// FUNCIONES
-const crearMascota = function () {
-  let nombre = prompt("Ingrese el nombre de la mascota: ");
-  let tipo = prompt("Ingrese el tipo de la mascota: ");
-  let sexo = prompt("Ingrese el sexo de la mascota: ");
-  let raza = prompt("Ingrese la raza de la mascota: ");
-  let color = prompt("Ingrese el color de la mascota: ");
-  let descripcion = prompt("Ingrese una breve descripcion de la mascota: ");
-
-  let mascotaNueva = new Mascota(nombre, tipo, sexo, raza, color, descripcion);
-  agregarMascotaDOM(mascotaNueva);
-  return mascotaNueva;
-};
-
-const cantidadDeMascotasEnAdopcion = () => mascotasEnAdopcion.length;
-const agregarMascotaEnAdopcion = () => {
-  mascotasEnAdopcion.push(crearMascota());
-};
-const filtrarPorTipo = (tipo) =>
-  mascotasEnAdopcion.filter((mascota) => mascota.tipo === tipo);
-
-const buscarMascotaPorNombre = (nombre) =>
-  mascotasEnAdopcion.find((mascota) => mascota.nombre === nombre);
-
-const eliminarMascota = function (mascota) {
-  const index = mascotasEnAdopcion.indexOf(mascota);
-  if (index > -1) {
-    mascotasEnAdopcion.splice(index, 1);
-    sacarMascotaDelDOM(index);
-  }
-
-  localStorage.removeItem(mascota.nombre);
-};
-
-/* DOM */
-const $menu = document.querySelector("#menu");
-const $mascotasTotales = document.querySelector(".mascotas-totales");
-const $formMascotas = document.querySelector(".form-poner-en-adopcion");
-
-const crearCardsMascotas = function () {
-  if (localStorage.length === 0) {
-    for (let mascota of mascotasEnAdopcion) {
-      agregarMascotaDOM(mascota);
+          <strong>Title: </strong> ${informacion.name}
+          <br />
+          <strong>Episode: </strong> ${informacion.episode}
+          <br />
+          <strong>Air-date: </strong> ${informacion.air_date}
+          <br />
+        </div>
+        `);
+      }
     }
-  } else {
-    Object.keys(localStorage).forEach(function (key) {
-      let mascota = JSON.parse(localStorage.getItem(key));
-      crearCardMascotaParticular(mascota);
-    });
-  }
-};
-
-const sacarMascotaDelDOM = function (indexMascota) {
-  $mascotasTotales.removeChild($mascotasTotales.childNodes[indexMascota]);
-  console.log($mascotasTotales.childNodes[indexMascota]);
-};
-
-const crearCardMascotaParticular = (mascota) => {
-  let card = document.createElement("div");
-  card.innerHTML = `<div
-    class="card col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4"
-    style="width: 18rem"
-  >
-    <div class="card-body">
-      <h5 class="card-title">${mascota.nombre}</h5>
-      <p class="card-text">${mascota.descripcion}</p>
-      <button type="submit" class="btn btn-primary ${mascota.nombre}"
-        >ADOPTAR</button
-      >
-    </div>
-  </div>`;
-
-  /*   $mascotasTotales.appendChild(card); */
-
-  $(".mascotas-totales").append(card);
-
-  /* const $botonAdoptar = document.querySelector(`.${mascota.nombre}`);
-  $botonAdoptar.addEventListener("click", () => {
-    eliminarMascota(mascota);
-  }); */
-
-  $(`.${mascota.nombre}`).click(() => {
-    eliminarMascota(mascota);
-  });
-};
-
-const agregarMascotaDOM = function (mascota) {
-  crearCardMascotaParticular(mascota);
-  localStorage.setItem(`${mascota.nombre}`, JSON.stringify(mascota));
-};
-
-/* const $botonSubmit = document.querySelector(".enviar-form");
-$botonSubmit.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  let nombre = document.querySelector(".nombre").value;
-  let tipo = document.querySelector(".tipo").value;
-  let sexo = document.querySelector(".sexo").value;
-  let raza = document.querySelector(".raza").value;
-  let color = document.querySelector(".color").value;
-  let descripcion = document.querySelector(".descripcion").value;
-
-  agregarMascotaDOM(new Mascota(nombre, tipo, sexo, raza, color, descripcion));
-}); */
-
-const $botonSubmit = $(".formulario-adopcion");
-$botonSubmit.submit(function (e) {
-  e.preventDefault();
-  let hijos = $(e.target).children();
-
-  console.log(hijos);
-
-  agregarMascotaDOM(
-    new Mascota(
-      hijos[1].value,
-      hijos[4].value,
-      hijos[7].value,
-      hijos[10].value,
-      hijos[13].value,
-      hijos[16].value
-    )
   );
 });
-
-// FUNCIONES BOTONES MENU
-/* const cambiarVentanas = () => {
-  $mascotasTotales.classList.toggle("oculto");
-  $formMascotas.classList.toggle("oculto");
-};
-
-let $botonMostrarMascotas = document.querySelector("#mostrar-mascotas");
-let $botonFormAdoptar = document.querySelector("#poner-adopcion-mascota");
-
-$botonMostrarMascotas.addEventListener("click", cambiarVentanas);
-$botonFormAdoptar.addEventListener("click", cambiarVentanas); */
-
-$("#mostrar-mascotas").click(() => {
-  $(".mascotas-totales").toggle("slow");
-});
-
-$("#poner-adopcion-mascota").click(() => {
-  $(".form-poner-en-adopcion").toggle("fast");
-});
-
-// MAIN
-crearCardsMascotas();
